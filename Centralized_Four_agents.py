4314216202c7245ab91b0d67c3d89a4e62f1241a
niddipi <neelesh.iddipilla@okstate.edu>
2018-09-04 09:41:00 -0500

    These changes help the uavs to work heterogenously along with turtlebots.This is a Centralized approach where the algorithm calculate tasks for all the agents once and assign to them.

---
+++ Centralized_Four_agents.py
@@ -0,0 +1,288 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from pulp import *
+import numpy as np
+import math
+
+##Locations of the tasks
+locations = [[0, 0], [0, 1], [0, 2], [0, 3],[0,4],[0,5],[1, 0], [1, 1], [1, 2], [1, 3],[1,4],[1,5],[2, 0], [2, 1], [2, 2], [2, 3], [2,4], [2,5],[3, 0], [3, 1], [3, 2], [3, 3],[3,4],[3,5],[4, 0], [4, 1], [4, 2], [4, 3],[4,4],[4,5]]
+#Task names to be added as nodes in graph
+tasks = [0 ,1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19 ,20 ,21 ,22 ,23,24,25,26,27,28,29]
+
+n = len(locations)
+#Reward(R) for the task
+reward = {"agent1":[],"agent2":[],"agent3":[],"agent4":[]}
+reward["agent1"] = [1,2,1,1,1,1,4,1,1,1,1,3,1,1,5,1,4,1,1,1,2,1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+reward["agent2"] = [1,1,2,1,2,1,1,1,1,1,1,0.2,1,1,0.5,1,4,1,1,1,2,1,1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+reward["agent3"] = [0.1,0.1,0.1,0.1,0.1,0.1,0.1,1,1,1,1,0.5,1,4,1,1,1,2,2,2,2,2,2,2,2,2,4,4,4,4]
+reward["agent4"] = [0.1,0.1,0.1,0.1,0.1,0.1,0.1,1,2,2,2,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,3,3]
+
+
+#Efficiency(psi) of the task
+efficiency = {"agent1":[],"agent2":[],"agent3":[],"agent4":[]}
+#efficiency["agent1"] = [0.3,1,0.1,0.2,0.1,0.5,0.6,0.2,0.7,0.6,0.5,0.6,0.3,0.1,0.3,0.4,0.2,1,0.2,0.5,0.2,0.6,0.6,1]
+efficiency["agent1"]= [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+#efficiency["agent2"] = [0.3,0.1,0.4,0.2,0.1,0.5,0.6,0.2,0.9,0.6,0.5,0.6,0.3,0.9,0.3,0.4,0.2,1,0.2,0.5,1,0.6,0.6,0.1]
+efficiency["agent2"]=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+efficiency["agent3"]=[4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]
+efficiency["agent4"]=[4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]
+
+
+""" Dummy task is represented by index n"""
+DUMMY = n
+
+G = {"agent1":nx.DiGraph(),"agent2":nx.DiGraph(),"agent3":nx.DiGraph(),"agent4":nx.DiGraph()}
+
+G["agent1"].add_nodes_from(tasks)
+G["agent2"].add_nodes_from(tasks)
+G["agent3"].add_nodes_from(tasks)
+G["agent4"].add_nodes_from(tasks)
+#Edges of the graph
+Edges1 = [(0, 1),(1, 7),(0,6),(6,12),(12,13),(7, 13),(13, 19),(19, 20),(20, 21),(21,15),(15,14),(14,8),(8,2),(2,1)]
+Edges2 = [(5, 11),(11, 17),(17,16),(16,10),(10,4),(4,3),(9, 15),(15, 21),(21, 22),(22,23),(23,17)]
+Edges3 = [(11, 17),(11,10),(10,16),(16,17),(17,23),(23,22),(22,21),(21,20),(20, 19),(19, 18),(18, 12),(12,13),(13,7),(7,1)]
+Edges4 = [(11,17),(17,23),(23,29),(29,28),(28,22),(22,21),(21,27),(27,26),(26,25),(25,24),(24,18),(18,12),(12,13),(13,14)]
+
+
+G["agent1"].add_edges_from(Edges1)
+G["agent2"].add_edges_from(Edges2)
+G["agent3"].add_edges_from(Edges3)
+G["agent4"].add_edges_from(Edges4)
+
+##Assigned time for the task
+w = {"agent1":[],"agent2":[],"agent3":[],"agent4":[]}
+
+
+
+x = {"agent1":{},"agent2":{},"agent3":{},"agent4":{}}
+
+""" EF. add variable y """
+y ={"agent1":{},"agent2":{},"agent3":{},"agent4":{}}
+
+G["agent1"].add_edge(DUMMY,0)
+G["agent1"].add_edge(DUMMY,6)
+G["agent2"].add_edge(DUMMY,5)
+G["agent3"].add_edge(DUMMY,11)
+G["agent4"].add_edge(DUMMY,17)
+G["agent4"].add_edge(DUMMY,11)
+
+#Addition of Dummy edges to graph
+for key in x:
+	for i in range(0,len(tasks)):
+		if len(G[key].in_edges(i)) != 0 or len(G[key].out_edges(i)) != 0:
+			G[key].add_edge(i,DUMMY)
+			#G[key].add_edge(DUMMY,i)
+
+#Defining the problem statement
+def Best_tasks_for_agent(Completion_map,T):
+	prob = pulp.LpProblem('Task allocation', pulp.LpMaximize)
+
+	#Defining xij variables
+	for key in x :
+		for i in range(0,n)+[DUMMY]:
+			for j in range(0,n)+[DUMMY]:
+				lp_name = key+"x"+'_'+ str(i) + '_' + str(j)
+				lowerBound = 0
+				upperBound = 1
+				if i != j and G[key].has_edge(i,j):
+					x[key][i,j] = pulp.LpVariable(
+								lp_name, lowerBound,upperBound,pulp.LpBinary)
+				""" EF. add variable y_ik """
+			if len(G[key].in_edges(i)) != 0 or len(G[key].out_edges(i)) != 0 or i == DUMMY:
+				y[key][i] = pulp.LpVariable(
+						key+"y_" + str(i), lowerBound, upperBound,pulp.LpBinary)
+
+	#Conditions 2,3, and 4
+	for key in x :
+		for i in range(0, n):
+			""" EF. add variable y """
+			if len(G[key].in_edges(i)) != 0 or len(G[key].out_edges(i)) != 0:
+				prob += pulp.lpSum([x[key][i,j] for j in range(0,n)+[DUMMY] 
+							if i!=j and G[key].has_edge(i,j)]) == y[key][i],""
+		prob += y[key][DUMMY] == 1
+
+	for key in x :
+		for j in range(0, n):
+			""" EF. add variable y, range(1,n) should be range(0,n)""" 
+			if len(G[key].in_edges(j)) != 0 or len(G[key].out_edges(j)) != 0:
+				prob += pulp.lpSum([x[key][i,j] for i in range(0,n)+[DUMMY] 
+							if i!=j and G[key].has_edge(i,j)]) == y[key][j]
+
+	t = {"agent1":[],"agent2":[],"agent3":[],"agent4":[]}
+	for key in x:
+		for i in range(0, n)+[DUMMY]:
+			t[key].append(pulp.LpVariable(key+'t_' + str(i), cat='Integer'))
+			w[key].append(pulp.LpVariable(key+'w_' + str(i), cat='Integer'))
+	print "T is :",T
+	for key in x:
+		prob += pulp.lpSum([w[key][i] for i in range(0,n)+[DUMMY]]) <= T
+	##Condition 5 where T = 5(Missions intervals assigned to the task)
+	for key in x:
+		for i in range(0, n)+[DUMMY]:
+			for j in range(0, n)+[DUMMY]:
+				if i != j and G[key].has_edge(i,j):
+					prob += ((t[key][i] + w[key][i] - t[key][j]) <= (1-x[key][i,j])*(T+1))
+
+
+	""" EF. constraints (3) and (4) must be specified """
+	for key in x:
+		prob += pulp.lpSum([x[key][i,DUMMY] for i in range(0,n) 
+							if G[key].has_edge(i,DUMMY)]) == 1,""
+		prob += pulp.lpSum([x[key][DUMMY,i] for i in range(0,n) 
+							if G[key].has_edge(DUMMY,i)]) == 1,""
+
+	service = {"agent1":[],"agent2":[],"agent3":[],"agent4":[]} #phi variable
+	""" EF. service should be continuous """
+	for key in x:
+		for i in range(0, n):
+		    service[key].append(pulp.LpVariable(key+'phi_' + str(i), cat='Continuous'))
+
+	#condition 7 for each task service is efficiency * assigned time
+	for key in x:
+		for i in range(0,n):
+			if len(G[key].in_edges(i)) != 0 or len(G[key].out_edges(i)) != 0:
+					#prob += service[key][i] <= pulp.lpSum(
+					#	[efficiency[ag][i]*w[ag][i] for ag in x])
+					prob += service[key][i] <= efficiency[key][i]*w[key][i]
+					prob += y[key][i] <= t[key][i]
+					prob += y[key][i] <= w[key][i]
+					prob += t[key][i] <= y[key][i]*(T+1)
+					prob += w[key][i] <= y[key][i]*(T+1)
+
+	#condition 8
+	Required_completion = 1
+
+	##I meant Required completion as Cm 
+	for key in x:
+		for i in range(0,n):
+			prob += service[key][i]<= 1 
+			""" EF. some other constraint. You could also set it as bound """
+			prob += 0 <= service[key][i]
+
+
+
+	#Objective Function condition 1
+	prob += pulp.lpSum([reward[key][k] * service[key][k] for key in x for k in range(0,n)])	
+
+
+	prob.writeLP("result.lp")
+	optimization_result = prob.solve()
+
+	print("Status:", LpStatus[prob.status])
+	print("Optimal Solution to the problem: ", value(prob.objective))
+	print ("Individual decision_variables: ")
+	prob.writeLP("result.lp")
+
+	variable_name = []
+        task = {"agent1_task_route":[],"agent1_task_route_points":[],
+		"agent2_task_route":[],"agent2_task_route_points":[],
+		"agent3_task_route":[],"agent3_task_route_points":[],
+		"agent4_task_route":[],"agent4_task_route_points":[],}
+	agent1_task_route = []
+	agent2_task_route = []
+	agent3_task_route = []
+	agent4_task_route = []
+	agent1_task_route_points = []
+	agent2_task_route_points = []
+	agent3_task_route_points = []
+	agent4_task_route_points = []
+
+	for v in prob.variables():
+		if(v.varValue > 0):
+			print(v.name + "= " + str(v.varValue))
+			variable_name.append(v.name) 
+
+	#Extracts task loactions from the arc
+        temp = "30"
+        for num in range(len(variable_name)):
+                data = variable_name[num].split("_")
+                if data[0] == "agent1x":
+                        for i in range(len(variable_name)):
+                                data = variable_name[i].split("_")
+                                if data[1] == temp and data[0] == "agent1x":
+                                        agent1_task_route.append(data[1])
+                                        temp = data[2]
+                                        break
+	  ##Indexes of the points in locations defined
+        print "Indexes of the points in task locations list defined"
+        ###Prints the task locations.
+        for i in range(0,len(agent1_task_route)):
+                if agent1_task_route[i] != "30":
+                        agent1_task_route_points.append(locations[int(agent1_task_route[i])])
+        task["agent1_task_route"] = agent1_task_route
+        task["agent1_task_route_points"]= agent1_task_route_points
+
+
+	#Extracts task loactions from the arc
+        temp = "30"
+        for num in range(len(variable_name)):
+                data = variable_name[num].split("_")
+                if data[0] == "agent2x":
+                        for i in range(len(variable_name)):
+                                data = variable_name[i].split("_")
+                                if data[1] == temp and data[0] == "agent2x":
+					print data
+                                        agent2_task_route.append(data[1])
+                                        temp = data[2]
+                                        break
+	  ##Indexes of the points in locations defined
+        print "Indexes of the points in task locations list defined"
+        ###Prints the task locations.
+        for i in range(0,len(agent2_task_route)):
+                if agent2_task_route[i] != "30":
+                        agent2_task_route_points.append(locations[int(agent2_task_route[i])])
+        task["agent2_task_route"] = agent2_task_route
+        task["agent2_task_route_points"]= agent2_task_route_points
+	
+	#Extracts task loactions from the arc
+        temp = "30"
+        for num in range(len(variable_name)):
+                data = variable_name[num].split("_")
+                if data[0] == "agent3x":
+                        for i in range(len(variable_name)):
+                                data = variable_name[i].split("_")
+                                if data[1] == temp and data[0] == "agent3x":
+					print data
+                                        agent3_task_route.append(data[1])
+                                        temp = data[2]
+                                        break
+	  ##Indexes of the points in locations defined
+        print "Indexes of the points in task locations list defined"
+        ###Prints the task locations.
+        for i in range(0,len(agent3_task_route)):
+                if agent3_task_route[i] != "30":
+                        agent3_task_route_points.append(locations[int(agent3_task_route[i])])
+        task["agent3_task_route"] = agent3_task_route
+        task["agent3_task_route_points"]= agent3_task_route_points
+	
+	#Extracts task loactions from the arc
+        temp = "30"
+        for num in range(len(variable_name)):
+                data = variable_name[num].split("_")
+                if data[0] == "agent4x":
+                        for i in range(len(variable_name)):
+                                data = variable_name[i].split("_")
+                                if data[1] == temp and data[0] == "agent4x":
+					print data
+                                        agent4_task_route.append(data[1])
+                                        temp = data[2]
+                                        break
+	  ##Indexes of the points in locations defined
+        print "Indexes of the points in task locations list defined"
+        ###Prints the task locations.
+        for i in range(0,len(agent4_task_route)):
+                if agent4_task_route[i] != "30":
+                        agent4_task_route_points.append(locations[int(agent4_task_route[i])])
+        task["agent4_task_route"] = agent4_task_route
+        task["agent4_task_route_points"]= agent4_task_route_points
+        return task
+'''
+Completion_map = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+T = 5
+task = Best_tasks_for_agent(Completion_map,T)
+print task
+pos1 = nx.spring_layout(G["agent2"],k=0.7,iterations=20)
+nx.draw(G["agent2"],pos1,with_labels=True)
+plt.title('Traversability graph given')
+plt.show()
+'''
---
+++ Move_drones_turtlebots.py
@@ -0,0 +1,228 @@
+import rospy
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Point
+from math import atan2
+from Centralized_Multiple_agents import Best_tasks_for_agent 
+from independent_study.msg import IntList
+from independent_study.msg import agent3_path
+from independent_study.msg import agent4_path
+from independent_study.msg import uav1
+from independent_study.msg import uav2
+import threading
+
+
+rospy.init_node('Move_drones_turtlebots')
+T = 5
+Completion_map = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+count = 0
+#Robot1 parameters
+robot1_x = 0.0
+robot1_y = 0.0
+robot1_angle = 0.0
+
+#UAV updates
+uav1_COMPLETED=""
+uav2_COMPLETED=""
+
+#Robot2 parameters
+robot2_x = 0.0
+robot2_y = 0.0
+robot2_angle = 0.0
+
+#task
+task = {"agent1_task_route":[],"agent1_task_route_points":[],
+	"agent2_task_route":[],"agent2_task_route_points":[],
+	"agent3_task_route":[],"agent3_task_route_points":[],
+	"agent4_task_route":[],"agent4_task_route_points":[]}
+
+
+def R1Cur_loc(msg):
+	global robot1_x
+	global robot1_y
+	global robot1_angle
+	robot1_x = msg.pose.pose.position.x
+	robot1_y = msg.pose.pose.position.y
+	robot1_orient = msg.pose.pose.orientation
+	(roll,pitch,robot1_angle)=euler_from_quaternion(
+		[robot1_orient.x,robot1_orient.y,robot1_orient.z,robot1_orient.w])
+
+
+def R2Cur_loc(msg):
+	global robot2_x
+	global robot2_y
+	global robot2_angle
+	robot2_x = msg.pose.pose.position.x
+	robot2_y = msg.pose.pose.position.y
+	robot2_orient = msg.pose.pose.orientation
+	(roll,pitch,robot2_angle)=euler_from_quaternion(
+		[robot2_orient.x,robot2_orient.y,robot2_orient.z,robot2_orient.w])
+
+def uav1_update(msg):
+	global uav1_COMPLETED
+	uav1_COMPLETED = msg	
+	print "uav1_compl :",uav1_COMPLETED
+
+def uav2_update(msg):
+	global uav2_COMPLETED 
+	uav2_COMPLETED = msg.COMPLETED
+
+rospy.loginfo("Welcome")
+
+#This is for the First robot
+Odom_sub1 = rospy.Subscriber(
+	'/robot1/odom',Odometry,R1Cur_loc)
+
+cmd_vel_pub1 = rospy.Publisher(
+	'/robot1/mobile_base/commands/velocity', Twist, queue_size=10)
+
+
+#This is for the second robot
+Odom_sub2 	= rospy.Subscriber('/robot2/odom',Odometry,R2Cur_loc)
+cmd_vel_pub2  	= rospy.Publisher('/robot2/mobile_base/commands/velocity', Twist, queue_size=10)
+uav1_path_pub = rospy.Publisher('agent3_path',agent3_path, queue_size=10)
+uav2_path_pub = rospy.Publisher('agent4_path',agent4_path, queue_size=10)
+uav1_sub      = rospy.Subscriber('uav1',uav1,uav1_update)
+uav2_sub      = rospy.Subscriber('uav2',uav2,uav2_update)
+
+rospy.loginfo("done")
+rate = rospy.Rate(10)
+
+R1move  = Twist()
+R1_Dest  = Point()
+
+
+R2move  = Twist()
+R2_Dest  = Point()
+
+
+def Myturtlebot1(agent1_task_route_points):
+	global R1_Dest
+	global R1_move
+	global robot1_x
+        global robot1_y
+        global robot1_angle
+	global count
+	i =0
+	if count == 0:
+		count =1
+		#print agent1_task_route_points
+		count = 0
+	while i < len(agent1_task_route_points):
+		R1_Dest.x = agent1_task_route_points[i][0]
+	        R1_Dest.y = agent1_task_route_points[i][1]
+		while not rospy.is_shutdown():
+			R1x_diff = R1_Dest.x - robot1_x
+			R1y_diff = R1_Dest.y - robot1_y
+			R1new_angle = atan2(R1y_diff,R1x_diff)
+			if abs(R1new_angle - robot1_angle)> 0.2:
+				R1move.linear.x  = 0.0
+				R1move.angular.z = 0.3
+			else:
+				R1move.linear.x = 0.5
+				R1move.linear.z = 0.0
+
+			cmd_vel_pub1.publish(R1move)
+			rate.sleep()
+			R1x_diff = abs(R1_Dest.x - robot1_x)
+			R1y_diff = abs(R1_Dest.y - robot1_y)
+			if(R1x_diff < 0.2 and R1y_diff < 0.2):
+				i = i+1
+                        	break
+
+def Myturtlebot2(agent2_task_route_points):
+	i =0
+	global R2_Dest
+	global R2_move
+	global robot2_x
+        global robot2_y
+        global robot2_angle
+	global count
+	if count == 0:
+		count =1
+		count = 0
+	while i < len(agent2_task_route_points):
+		R2_Dest.x = agent2_task_route_points[i][0]
+		R2_Dest.y = agent2_task_route_points[i][1]
+		while not rospy.is_shutdown():
+			R2x_diff = R2_Dest.x - robot2_x
+			R2y_diff = R2_Dest.y - robot2_y
+			R2new_angle = atan2(R2y_diff,R2x_diff)
+			if abs(R2new_angle - robot2_angle)> 0.2:
+				R2move.linear.x  = 0.0
+				R2move.angular.z = 0.2
+			else:
+				R2move.linear.x = 0.5
+				R2move.linear.z = 0.0
+			
+			cmd_vel_pub2.publish(R2move)
+			rate.sleep()
+			R2x_diff = abs(R2_Dest.x - robot2_x)
+			R2y_diff = abs(R2_Dest.y - robot2_y)
+			'''
+			print "R2_Dest :",R2_Dest.x
+			print "R2_Dest :",R2_Dest.y
+			print "X :",robot2_x
+			print "Y :",robot2_y
+			print "R2_xdiff :",R2x_diff
+			print "R2_ydiff :",R2y_diff
+			'''
+			if(R2x_diff < 0.2 and R2y_diff < 0.2):
+				i = i+1
+                        	break
+
+def publish_waypoints_uav1(agent3_task_route_points):
+	global uav1_COMPLETED
+	print " uav1_COMPLETED :",uav1_COMPLETED
+	while(True):
+		uav1_path_pub.publish(agent3_task_route_points)
+		''''
+		if(uav1_COMPLETED == "yes"):
+			break
+		'''
+def publish_waypoints_uav2(agent4_task_route_points):
+	global uav2_COMPLETED
+	while(True):
+		uav2_path_pub.publish(agent4_task_route_points)
+		'''
+		if(uav2_COMPLETED == "yes"):
+			break
+		'''
+
+task    = Best_tasks_for_agent(Completion_map,T)
+agent1_task_route        = task["agent1_task_route"]
+agent1_task_route_points = task["agent1_task_route_points"]
+agent2_task_route        = task["agent2_task_route"]
+agent2_task_route_points = task["agent2_task_route_points"]
+agent3_task_route        = task["agent3_task_route"]
+agent3_task_route_points = task["agent3_task_route_points"]
+agent4_task_route        = task["agent4_task_route"]
+agent4_task_route_points = task["agent4_task_route_points"]
+
+thread1 = threading.Thread(target=Myturtlebot1, args=[agent1_task_route_points])
+thread2 = threading.Thread(target=Myturtlebot2, args=[agent2_task_route_points])
+thread3 = threading.Thread(target=publish_waypoints_uav1, args=[agent3_task_route_points])
+thread4 = threading.Thread(target=publish_waypoints_uav2, args=[agent4_task_route_points])
+
+for p in range(0,len(agent1_task_route)):
+	if agent1_task_route[p] != "30":
+		Completion_map[int(agent1_task_route[p])] = 0
+for p in range(0,len(agent2_task_route)):
+	if agent2_task_route[p] != "30":
+		Completion_map[int(agent2_task_route[p])] = 0
+for p in range(0,len(agent3_task_route)):
+	if agent3_task_route[p] != "30":
+		Completion_map[int(agent3_task_route[p])] = 0
+for p in range(0,len(agent4_task_route)):
+	if agent4_task_route[p] != "30":
+		Completion_map[int(agent4_task_route[p])] = 0
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
